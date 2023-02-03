7415d59f4b35b7fc75e18d715c886ab2fdf099c2
stevenkim18 <11882@naver.com>
2023-02-03 13:12:09 +0900

    provider overview 10(ch.17)

--- Udemy/provider_essential/provider_overview_4/lib/main.dart
+++ Udemy/provider_essential/provider_overview_4/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'models/dog.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,62 +12,117 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Provider 0',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider<Dog>(
+      create: (context) => Dog(name: 'dog10', breed: 'breed10', age: 3),
+      child: MaterialApp(
+        title: 'Provider 10',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class Foo with ChangeNotifier {
-  String value = "Foo";
-
-  void changeValue() {
-    value = value == "Foo" ? "Bar" : "Foo";
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Provider 09"),
+        title: const Text('Provider 10'),
       ),
-      body: ChangeNotifierProvider<Foo>(
-        create: (_) => Foo(),
-        child: Consumer<Foo>(
-          builder: ((context, foo, _) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    foo.value,
-                    style: const TextStyle(fontSize: 40),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                      onPressed: () => foo.changeValue(),
-                      child: const Text(
-                        "Change value",
-                        style: TextStyle(fontSize: 20.0),
-                      ))
-                ],
-              ),
-            );
-          }),
+      // 여기!!!
+      body: Selector<Dog, String>(
+        // 여기!!!
+        selector: (context, dog) => dog.name,
+        builder: (BuildContext context, String name, Widget? child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                child!,
+                const SizedBox(height: 10.0),
+                Text(
+                  '- name: $name', // 매개변수로 받은 name!
+                  style: const TextStyle(fontSize: 20.0),
+                ),
+                const SizedBox(height: 10.0),
+                const BreedAndAge(),
+              ],
+            ),
+          );
+        },
+        child: const Text(
+          'I like dogs very much',
+          style: TextStyle(fontSize: 20.0),
         ),
       ),
+    );
+  }
+}
+
+class BreedAndAge extends StatelessWidget {
+  const BreedAndAge({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<Dog, String>(
+      selector: (context, dog) => dog.breed,
+      builder: (_, String breed, __) {
+        return Column(
+          children: [
+            Text(
+              '- breed: $breed',
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            const SizedBox(height: 10.0),
+            const Age(),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Age extends StatelessWidget {
+  const Age({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<Dog, int>(
+      selector: (context, dog) => dog.age,
+      builder: (_, age, __) {
+        return Column(
+          children: [
+            Text(
+              '- age: $age',
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () => context.read<Dog>().grow(), // dog가 없기 때문에 context에서 가져옴.
+              child: const Text(
+                'Grow',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
